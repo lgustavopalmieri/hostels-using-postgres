e4e5fdcd74196ac27d738807b1175aa96b6c16e3
lgustavopalmieri <lgustavopalmieri@gmail.com>
2022-10-18 13:54:31 -0300

    feat: refact dependencies and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import getConfig from './config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const config = getConfig();
+
+const DATABASE_SETUP = [
+  ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+  TypeOrmModule.forRoot({
+    ...(config.database as TypeOrmModuleOptions),
+    entities: ['dist/modules/**/entities/**.entity{.ts,.js}'],
+    migrations: ['dist/database/migrations/**/*.js'],
+    migrationsTableName: 'migrations_store-api',
+    migrationsRun: true,
+  }),
+];
+
 @Module({
-  imports: [],
+  imports: [...DATABASE_SETUP],
   controllers: [AppController],
   providers: [AppService],
 })
